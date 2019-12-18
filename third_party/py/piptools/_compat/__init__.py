fc9e3c9889c9c2a1f84b113dd0716e95dc056734
Ali Shariat <shariat@gmail.com>
2019-12-18 02:45:11 -0800

    vendor dependency

---
+++ third_party/py/piptools/_compat/__init__.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+# flake8: noqa
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import six
+
+from .pip_compat import (
+    DEV_PKGS,
+    FAVORITE_HASH,
+    PIP_VERSION,
+    FormatControl,
+    InstallationCandidate,
+    InstallCommand,
+    InstallRequirement,
+    Link,
+    PackageFinder,
+    PyPI,
+    RequirementSet,
+    RequirementTracker,
+    Resolver,
+    Wheel,
+    WheelCache,
+    cmdoptions,
+    get_installed_distributions,
+    install_req_from_editable,
+    install_req_from_line,
+    is_dir_url,
+    is_file_url,
+    is_vcs_url,
+    parse_requirements,
+    path_to_url,
+    stdlib_pkgs,
+    url_to_path,
+    user_cache_dir,
+)
+
+if six.PY2:
+    from .tempfile import TemporaryDirectory
+else:
+    from tempfile import TemporaryDirectory
