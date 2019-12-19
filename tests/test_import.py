120a23efaa05e01fdd26b399c0986b660dfbc6e8
Ali Shariat <shariat@gmail.com>
2019-12-18 23:22:13 -0800

    cleanup tests requirements

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,8 +1,5 @@
 def test_import():
     # just try to import some packages that use namespace
     import google.cloud.language
-    import testing.postgresql
-    import testing.mysqld
-    import tensorflow as tf
-    import azure.common
     import azure.storage.blob
+    import dateutil
