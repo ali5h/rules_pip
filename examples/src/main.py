5666c6f1f0714a993952d88dc39c21d7a20c8cf4
Ali Shariat <shariat@gmail.com>
2020-01-15 11:38:28 -0800

    add py3_image example

---
+++ examples/src/main.py
@@ -0,0 +1,12 @@
+from google.cloud import language_v1 as lang
+
+client = lang.LanguageServiceClient()
+
+document = lang.types.Document(
+    content='Google, headquartered in Mountain View, unveiled the '
+            'new Android phone at the Consumer Electronic Show.  '
+            'Sundar Pichai said in his keynote that users love '
+            'their new Android phones.',
+    language='en',
+    type='PLAIN_TEXT',
+)
