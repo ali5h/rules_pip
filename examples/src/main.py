34c407a2e791a5b6a99fd685d233e94a891a2f61
Ali <320322+ali5h@users.noreply.github.com>
2020-08-16 23:55:24 -0700

    add formatting test for python code (#46)

--- examples/src/main.py
+++ examples/src/main.py
@@ -3,10 +3,10 @@ from google.cloud import language_v1 as lang
 client = lang.LanguageServiceClient()
 
 document = lang.types.Document(
-    content='Google, headquartered in Mountain View, unveiled the '
-            'new Android phone at the Consumer Electronic Show.  '
-            'Sundar Pichai said in his keynote that users love '
-            'their new Android phones.',
-    language='en',
-    type='PLAIN_TEXT',
+    content="Google, headquartered in Mountain View, unveiled the "
+    "new Android phone at the Consumer Electronic Show.  "
+    "Sundar Pichai said in his keynote that users love "
+    "their new Android phones.",
+    language="en",
+    type="PLAIN_TEXT",
 )
